6ab967e280593675430d6781068cb94758c58eee
ismael <ismael.maecar@educa.jcyl.es>
2021-10-20 15:46:21 +0000

    haciendo pr06

---
+++ PR06/codigo/ej_01.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+
+    <!-- Enlace al css -->
+    <link rel="stylesheet" href="../webroot/css/style.css">
+
+    <!-- @author - Ismael Maestre Carracedo  -->
+</head>
+<body>
+    
+    <!-- Ejercicio 1 -->
+    <h1>Ejercicio 1</h1>
+
+    <!-- Enunciado -->
+    <h2>Genera un array multidimensional y asociativo donde
+
+        <ul>
+            <li>los nombres de los equipos locales deben ser índices del array que contiene los
+                subarrays con el equipo visitante y a su vez un subarray con:
+
+                <ul>
+                    <li>resultado, roja, amarilla y penalti que son índices de los anterioes.</li>
+                </ul>
+            </li>
+        </ul>
+    </h2>
+
+    <!-- PHP -->
+    <?php
+
+    ?>
+
+</body>
+</html>
