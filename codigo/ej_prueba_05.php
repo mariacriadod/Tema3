3513e13e30dd8d0f4f433ab17751ae7ace6fa940
ismael <ismael.maecar@educa.jcyl.es>
2021-10-27 14:58:48 +0000

    años bisiestos

--- codigo/ej_prueba_05.php
+++ codigo/ej_prueba_05.php
@@ -30,7 +30,17 @@
     <!-- PHP -->
     <?php
     
-       
+        $anio = $_GET["anio"];
+
+        if(($anio%4 == 0) && (($anio%100 != 0) || ($anio%400 == 0)))
+        {
+            echo "El año ".$anio. " es bisiesto";
+        }
+        else
+        {
+            echo "El año ".$anio. " NO es bisiesto";
+        }
+            
     ?>
 
 </body>
